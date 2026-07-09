bb885044a39d8717685553eb08b8e619c99e96f7
anasrehmanworks <anasrehmanworks@gmail.com>
2026-07-09 05:07:12 +0500

    SMTP debug

--- lib/email.ts
+++ lib/email.ts
@@ -1,5 +1,11 @@
 import nodemailer from "nodemailer";
-
+console.log("=== SMTP DEBUG ===");
+console.log("HOST:", process.env.SMTP_HOST);
+console.log("PORT:", process.env.SMTP_PORT);
+console.log("USER:", process.env.SMTP_USER);
+console.log("PASS EXISTS:", !!process.env.SMTP_PASS);
+console.log("FROM:", process.env.EMAIL_FROM);
+console.log("==================");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
